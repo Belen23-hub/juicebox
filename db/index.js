f4fce1257f34ec0fc21075bab0214139cbb6fa60
Belén menganita <belenbahmerrada@gmail.com>
2020-12-12 09:59:00 -0600

    part one done

--- db/index.js
+++ db/index.js
@@ -19,8 +19,25 @@ async function getAllUsers() {
   return rows;
 }
 
-// and export them
+
+async function createUser({ username, password }) {
+  try {
+    const {rows} = await client.query(`
+      INSERT INTO users(username, password) 
+      VALUES($1, $2) 
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
+    `, [username, password]);
+
+    return rows;
+  
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   client,
   getAllUsers,
+  createUser
 }
